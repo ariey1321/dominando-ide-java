4e0ef629cac78e284abdb5d4fd4c3bc58312a49b
Ariele santos <ariely.dsantos251728@gmail.com>
2023-03-07 11:49:32 -0300

    firt commit

--- src/primeirop/PrimeiroPrograma.java
+++ src/primeirop/PrimeiroPrograma.java
@@ -6,20 +6,16 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 	 Gato gato = new Gato(); 
-	 Livros lIVROS = new Livros();
+	 
 	
 	 System.out.println(gato);
-	 System.out.println(lIVROS);
+	 
 	
 	/*	int a=2;
 	    int b=3;
 
 		System.out.println("Hello WORLD!" + (a+b));*/
 	}
-	}
-
-	class Livros{
-		private String nome;
-		private String npag;
+	
 	}
 
